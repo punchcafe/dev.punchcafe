eb37f2f7fec14f4da1c3a75fa77bce0fe8782876
Luca Fruzza <lfruzza@hotmail.co.uk>
2020-01-27 21:56:48 +0000

    :bento: add click animation

--- javascript.js
+++ javascript.js
@@ -1,79 +1,100 @@
 
 var pageState = {
   activeCartridge : null,
-  insertDelay : 300,
+  insertDelay : 800,
+  ejectDelay : 200,
   bufferLimitTop : 10,
   bufferLimitBottom : 700,
   bufferIncrement : 1,
-  bufferFrameDelay : 0.4
+  bufferFrameDelay : 0.4,
+  cartridgeFrame : "up"
 }
 
-var wholeThing = () => {
+var cartConfig = {
+   slideDist : 70,
+   slideFrameDelay : 3.5,
+   pause : 300,
+   clickDist : 30,
+   clickFrameDelay : 0.2
+}
 
-  //Have a global state of 'clicked'
-
-  var bufferHeight = 50;
-
-  var convertToCss = (int) => {
+var utilMethods = {
+  convertToCss : (int) => {
     return int.toString() + "px"
-  }
-
-  var convertFromCss = (string) => {
+  },
+  convertFromCss : (string) => {
     return parseInt(string.slice(0,-2))
   }
+}
 
-// function for '64 cartridge animation
+var portfolioPage = () => {
+
+  var bufferHeight = pageState.bufferLimitBottom;
+
+  var cartridges = document.getElementsByClassName("cartridge");
+  var cartridgesHolder = document.getElementsByClassName("cartridges")[0];
+
+  var top_buffer = document.getElementById("top-buffer");
+  console.log(top_buffer)
+  top_buffer.style.height = utilMethods.convertToCss(bufferHeight)
+
+
+  var expandContents = () => {
+    targetState = pageState.cartridgeFrame == "down" ? "up" : "down"
+    slideFrame(targetState, top_buffer)
+    pageState.cartridgeFrame = targetState
+  }
+
+  var slideFrame = (targetState, top_buffer) => {
+    // Slide frame holding cartridges up or down the screen.
+    var actions = (pageState.bufferLimitBottom - pageState.bufferLimitTop) / pageState.bufferIncrement;
+    var delay = pageState.insertDelay*1;
+    var increment = pageState.bufferIncrement * (targetState == "up" ? 1 : -1);
+    // make it go up or down
+    for(var i = 0; i < actions; i++){
+
+      setTimeout(()=>{
+        top_buffer.style.height = utilMethods.convertToCss(utilMethods.convertFromCss(top_buffer.style.height) + increment)
+      }, delay);
+
+      delay += pageState.bufferFrameDelay;
+    }
+  }
+
   var clickDown = (event) => {
+    // Provides '64 esque cartridge clicking
     var element = event.currentTarget;
-    if (state == "up"){
-    element.style.marginTop = convertToCss(70);
-  } else {
-    // Add click mechanism
-    element.style.marginTop = convertToCss(0);
+    if (pageState.cartridgeFrame == "up"){
+      // Insert cart
+      var cumulativeDelay = 0;
+      for(var i = 0; i < cartConfig.slideDist; i++){
+        // slide cart
+        setTimeout(()=>{
+          element.style.marginTop = utilMethods.convertToCss(utilMethods.convertFromCss(element.style.marginTop) + 1);
+          console.log(element.style.marginTop)
+        },cumulativeDelay);
+        cumulativeDelay += cartConfig.slideFrameDelay;
+      }
+      cumulativeDelay += cartConfig.pause
+      for(var i = 0; i < cartConfig.clickDist; i++){
+        setTimeout(()=>{
+          element.style.marginTop = utilMethods.convertToCss(utilMethods.convertFromCss(element.style.marginTop) + 1);
+        },cumulativeDelay);
+        cumulativeDelay += cartConfig.clickFrameDelay;
+      }
+    // element.style.marginTop = utilMethods.convertToCss(70);
+    } else {
+      // Add click mechanism
+      element.style.marginTop = utilMethods.convertToCss(0);
+    }
   }
-    console.log(element.style.height)
+
+  for( i = 0; i < cartridges.length; i++){
+    cartridges[i].addEventListener("click", clickDown);
+    cartridges[i].addEventListener("click", expandContents);
   }
 
 
-var cartridges = document.getElementsByClassName("cartridge");
-
-var cartridgesHolder = document.getElementsByClassName("cartridges")[0];
-
-var top_buffer = document.getElementById("top-buffer");
-top_buffer.style.height = convertToCss(bufferHeight)
-
-state = "up"
-
-var expandContents = () => {
-  targetState = state == "down" ? "up" : "down"
-  slideFrameTwo(targetState, top_buffer)
-  state = targetState
-  //top_buffer.style.height = top_buffer.style.height == "50px" ? "800px" : "50px";
 }
 
-var slideFrameTwo = (targetState, top_buffer) => {
-  var actions = (pageState.bufferLimitBottom - pageState.bufferLimitTop) / pageState.bufferIncrement;
-  var delay = pageState.insertDelay*1;
-  var increment = pageState.bufferIncrement * (targetState == "up" ? 1 : -1);
-  // make it go up or down
-  for(var i = 0; i < actions; i++){
-    setTimeout(()=>{
-      top_buffer.style.height = convertToCss(convertFromCss(top_buffer.style.height) + increment)
-    }, delay);
-    delay += pageState.bufferFrameDelay;
-  }
-  // calculate needed times before, then set that many timeouts with insertDelay
-}
-
-for( i = 0; i < cartridges.length; i++){
-  cartridges[i].addEventListener("click", clickDown);
-  cartridges[i].addEventListener("click", expandContents);
-}
-
-var insertCart = (cartridge) => {
-}
-
-
-}
-
-window.onload = wholeThing
+window.onload = portfolioPage
